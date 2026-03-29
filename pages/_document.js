b3b98f444e0afb4b51d4c69dc2263a1a783bb7cd
paulohenrique14 <paulobw012345@gmail.com>
2026-03-29 13:25:37 -0300

    feat(googleAdsense): added the snippets and the metatag

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,14 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="pt">
-        <Head/>
+        <Head>
+          <meta name="google-adsense-account" content="ca-pub-1718902470794481" />
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1718902470794481"
+            crossOrigin="anonymous"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
